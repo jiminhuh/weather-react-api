7af9f9882bcd24d706a76c02ec47598a6386e071
Jimin Huh <jiminhuh95@gmail.com>
2019-03-20 09:50:28 -0500

    created react app and added materialize for templating

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Row, Col } from "react-materialize";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default App;
